b5ce882e6e4a791e29f956da9172eaa52f9ee384
slepzov <slepzov@yandex.ru>
2022-08-16 23:44:10 +0300

    add blog on home page

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from .models import Blog
 
 
 def all_blogs(request):
-    blogs = Blog.objects.order_by('-date')[:5]
+    blogs = Blog.objects.order_by('-date')
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
 
 
